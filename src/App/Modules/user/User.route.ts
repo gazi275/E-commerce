fa86ebad3cb5a1e79a2a854daeeeee6854743f4f
gazi275 <gaziazad270@gmail.com>
2025-03-06 14:34:33 +0600

    user fatched successfully

--- src/App/Modules/user/User.route.ts
+++ src/App/Modules/user/User.route.ts
@@ -3,5 +3,7 @@ import { UserController } from "./User.controller";
 
 const router = Router();
 router.post("/signup", UserController.createUSer);
+router.get('/user', UserController.getAllUserController);
+router.get('/user/:id', UserController.getUSerController);
 
 export const UserRoute = router;
--- src/App/Modules/user/User.services.ts
+++ src/App/Modules/user/User.services.ts
@@ -6,6 +6,18 @@ const CreatUserServices = async (data: IUSer) => {
   return result;
 };
 
+const AllUserServices = async () => {
+  const result = await UserModel.find({ isDeleted: false }).lean();
+    return result;
+}
+
+const GetUserServices = async (id: string) => {
+  const result = await UserModel.findById({ id, isDeleted: false }).lean;
+  return result;
+};
+
 export const UserServices = {
   CreatUserServices,
+  GetUserServices,
+  AllUserServices
 };
